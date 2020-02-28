c3b09fca8c48887c6ef3b37752f4e46515f4fbc3
Eduardo <eduardo.nolla@outlook.com>
2020-02-28 10:38:54 +0100

    Query 2 done

--- LAB14/sql/eduardo.nolla_S14.sql
+++ LAB14/sql/eduardo.nolla_S14.sql
@@ -13,15 +13,17 @@ WHERE pm.id_movie = m.id_movie
 GROUP BY p.name
 HAVING COUNT(lm.id_movie) >
        (SELECT AVG((SELECT COUNT(lm2.id_movie)
-                    FROM movie AS m2,
-                         language_movie AS lm2
-                    WHERE m2.id_movie = lm2.id_movie
-                      AND m2.country = m.country
-                      AND m2.id_movie = m.id_movie
-                    GROUP BY m2.title
-                    ORDER BY COUNT(lm2.id_movie)
-                        DESC))
-        FROM movie AS m
+                      FROM movie AS m2,
+                           language_movie AS lm2
+                      WHERE m2.id_movie = lm2.id_movie
+                        AND m2.country = m.country
+                        AND m2.id_movie = m.id_movie
+                      GROUP BY m2.title
+                      ORDER BY COUNT(lm2.id_movie)
+                          DESC)) AS mediana
+        FROM movie AS m,
+             language_movie AS lm3
+       WHERE m.id_movie =lm3.id_movie
         GROUP BY m.country
         ORDER BY AVG((SELECT COUNT(lm2.id_movie)
                       FROM movie AS m2,
@@ -33,8 +35,8 @@ HAVING COUNT(lm.id_movie) >
                       ORDER BY COUNT(lm2.id_movie)
                           DESC))
             DESC
-        LIMIT 1);
-
+        LIMIT 1)
+ORDER BY p.name;
 
 
 
@@ -42,6 +44,28 @@ HAVING COUNT(lm.id_movie) >
 
 
 #&&2
+SELECT m.title, m.imdb_score
+FROM movie AS m
+WHERE m.gross > '60000000'
+  AND m.id_movie NOT IN (SELECT m2.id_movie
+                         FROM actor AS a2,
+                              person AS p2,
+                              movie AS m2,
+                              actor_movie AS am2
+                         WHERE a2.id_actor = p2.id_person
+                           AND m2.id_movie = am2.id_movie
+                           AND am2.id_actor = a2.id_actor
+                           AND p2.name not like 'A%'
+                         GROUP BY a2.id_actor)
+GROUP BY m.title, m.imdb_score
+ORDER BY m.imdb_score
+    DESC;
+
+
+
+
+
+
 
 #&&3
 
