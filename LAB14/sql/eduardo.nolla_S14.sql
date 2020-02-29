1c9cefc44c842b035ea8ee3ad0142deedb4aad26
Eduardo <eduardo.nolla@outlook.com>
2020-02-29 13:49:39 +0100

    Query 1 - problem

--- LAB14/sql/eduardo.nolla_S14.sql
+++ LAB14/sql/eduardo.nolla_S14.sql
@@ -10,34 +10,42 @@ FROM producer AS p,
 WHERE pm.id_movie = m.id_movie
   AND p.id_producer = pm.id_producer
   AND lm.id_movie = m.id_movie
+  AND (SELECT AVG((SELECT COUNT(lm2.id_movie)
+                       FROM movie AS m2,
+                            language_movie AS lm2
+                       WHERE m2.id_movie = lm2.id_movie
+                         AND m2.country = m.country
+                         AND m2.id_movie = m.id_movie
+                       GROUP BY m2.title
+                       ORDER BY COUNT(lm2.id_movie)
+                           DESC)) - 1 AS mediana
+       FROM movie AS m,
+            language_movie AS lm3
+       WHERE m.id_movie = lm3.id_movie
+       GROUP BY m.country
+       ORDER BY AVG((SELECT COUNT(lm2.id_movie)
+                     FROM movie AS m2,
+                          language_movie AS lm2
+                     WHERE m2.id_movie = lm2.id_movie
+                       AND m2.country = m.country
+                       AND m2.id_movie = m.id_movie
+                     GROUP BY m2.title
+                     ORDER BY COUNT(lm2.id_movie)
+                         DESC))
+           DESC
+       LIMIT 1)
+ <= ALL
+      (SELECT COUNT(lm2.id_language)
+       FROM movie AS m2,
+            producer_movie AS pm2,
+            language_movie AS lm2
+       WHERE m2.id_movie = pm2.id_movie
+         AND pm2.id_producer = p.id_producer #p.id_producer
+         AND lm2.id_movie = m2.id_movie
+       GROUP BY pm2.id_producer, m2.id_movie)
 GROUP BY p.name
-HAVING COUNT(lm.id_movie) >
-       (SELECT AVG((SELECT COUNT(lm2.id_movie)
-                    FROM movie AS m2,
-                         language_movie AS lm2
-                    WHERE m2.id_movie = lm2.id_movie
-                      AND m2.country = m.country
-                      AND m2.id_movie = m.id_movie
-                    GROUP BY m2.title
-                    ORDER BY COUNT(lm2.id_movie)
-                        DESC)) AS mediana
-        FROM movie AS m,
-             language_movie AS lm3
-        WHERE m.id_movie = lm3.id_movie
-        GROUP BY m.country
-        ORDER BY AVG((SELECT COUNT(lm2.id_movie)
-                      FROM movie AS m2,
-                           language_movie AS lm2
-                      WHERE m2.id_movie = lm2.id_movie
-                        AND m2.country = m.country
-                        AND m2.id_movie = m.id_movie
-                      GROUP BY m2.title
-                      ORDER BY COUNT(lm2.id_movie)
-                          DESC))
-            DESC
-        LIMIT 1)
-ORDER BY p.name;
-
+ORDER BY p.name
+LIMIT  5;
 
 #&&2
 SELECT m.title, m.imdb_score
@@ -58,6 +66,7 @@ ORDER BY m.imdb_score
     DESC;
 
 #&&3
+
 SELECT p.name
 FROM person AS p,
      actor AS a,
@@ -73,36 +82,21 @@ WHERE p.id_person = a.id_actor
        WHERE a3.id_actor = am3.id_actor
        GROUP BY am3.id_movie
        HAVING COUNT(am3.id_movie) >= 3)
-  AND (SELECT COUNT(*)
-       FROM ((SELECT gm2.id_genre
-              FROM movie AS m2,
-                   actor_movie AS am2,
-                   genre_movie AS gm2
-              WHERE am2.id_movie = m2.id_movie
-                AND gm2.id_movie = m2.id_movie
-              GROUP BY gm2.id_genre)) as generes) >= 15
+  AND 15 <= (SELECT gm2.id_genre
+             FROM movie AS m2,
+                  actor_movie AS am2,
+                  genre_movie AS gm2,
+                  actor AS a2
+             WHERE am2.id_movie = m2.id_movie
+               AND gm2.id_movie = m2.id_movie
+               AND a2.id_actor = am2.id_actor
+               AND a2.id_actor = '1023'
+             GROUP BY gm2.id_genre)
 GROUP BY p.name
 ORDER BY p.name;
 
-
-# (SELECT gm2.id_genre
-#               FROM movie AS m2,
-#                    actor_movie AS am2,
-#                    genre_movie AS gm2
-#               WHERE am2.id_movie = m2.id_movie
-#                 AND gm2.id_movie = m2.id_movie
-#                 AND am2.id_actor = '1023'
-#               GROUP BY gm2.id_genre)
-
-
-
-
-
-
 #&&4
-
-
-SELECT p.name
+SELECT p.name AS name
 FROM person AS p,
      director AS d,
      movie AS m,
@@ -115,22 +109,16 @@ WHERE p.id_person = d.id_director
                       WHERE m2.id_director = d.id_director
                       GROUP BY m2.id_movie)
   AND 10 <= ALL (SELECT COUNT(am2.id_movie)
-                     FROM movie AS m2,
-                          actor_movie AS am2
-                     WHERE m2.id_movie = am2.id_movie
-                       AND m2.id_director = d.id_director
-                     GROUP BY am2.id_movie)
+                 FROM movie AS m2,
+                      actor_movie AS am2
+                 WHERE m2.id_movie = am2.id_movie
+                   AND m2.id_director = d.id_director
+                 GROUP BY am2.id_movie)
 GROUP BY d.id_director, p.name
 ORDER BY p.name;
 
-
-
-
-
-
-
 #&&5
-SELECT p.name, pr.name, COUNT(p.name)
+SELECT p.name AS name, pr.name AS name, COUNT(p.name) AS num
 FROM person AS p,
      producer AS pr,
      director AS d,
