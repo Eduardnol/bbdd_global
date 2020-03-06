f0d6412d93b36ebe18f7870351e338721be2aabd
Eduardo <eduardo.nolla@outlook.com>
2020-03-06 21:24:20 +0100

    Query 5

--- LAB15/eduardo.nolla_S15.sql
+++ LAB15/eduardo.nolla_S15.sql
@@ -53,12 +53,56 @@ ORDER BY same DESC, p.name ASC
 LIMIT 10;
 
 #&&4
-SELECT m.title, pro.name, m.country AS country
+SELECT m.title, a.id_actor, pro.name AS producer, m.country AS country
 FROM movie AS m
-         LEFT JOIN actor_movie am on m.id_movie = am.id_movie,
-     producer AS pro
-         JOIN producer_movie pm on pro.id_producer = pm.id_producer
+         LEFT JOIN actor_movie am ON m.id_movie = am.id_movie
+         LEFT JOIN actor AS a ON am.id_actor = a.id_actor
+         JOIN producer_movie AS pm ON pm.id_movie = m.id_movie
+         JOIN producer AS pro ON pm.id_producer = pro.id_producer
 WHERE am.id_movie IS NULL
 GROUP BY m.id_movie, m.title
 ORDER BY m.title DESC
 LIMIT 5;
+
+# #&&4
+# SELECT m.title, a.id_actor, pro.name, m.country AS country
+# FROM movie AS m
+#          LEFT JOIN actor_movie am ON m.id_movie = am.id_movie
+#          LEFT JOIN actor AS a ON am.id_actor = a.id_actor,
+#      producer AS pro
+#          JOIN producer_movie pm ON pro.id_producer = pm.id_producer
+#          JOIN movie m2 ON pm.id_movie = m2.id_movie
+#
+# WHERE am.id_movie IS NULL AND m.id_movie = m2.id_movie
+# GROUP BY m.id_movie, m.title
+# ORDER BY m.title DESC
+# LIMIT 5;
+
+#&&5
+(SELECT a.id_actor, d.id_director, p.name, p.facebook_likes
+ FROM actor AS a
+          JOIN person AS p ON a.id_actor = p.id_person
+          JOIN actor_movie AS am ON a.id_actor = am.id_actor
+          JOIN movie AS m ON am.id_movie = m.id_movie
+          JOIN genre_movie AS gm ON m.id_movie = gm.id_movie
+          LEFT JOIN director AS d ON p.id_person = d.id_director
+ WHERE p.facebook_likes < (SELECT AVG(p.facebook_likes)
+                           FROM person AS p
+                                    JOIN actor a2 on p.id_person = a2.id_actor)
+ GROUP BY a.id_actor, p.facebook_likes
+ HAVING COUNT(DISTINCT gm.id_genre) > 17
+)
+UNION
+(SELECT a.id_actor, d.id_director, p.name, p.facebook_likes
+ FROM director AS d
+          JOIN person AS p ON d.id_director = p.id_person
+          JOIN movie AS m ON d.id_director = m.id_director
+          JOIN genre_movie AS gm ON m.id_movie = gm.id_movie
+          LEFT JOIN actor a on p.id_person = a.id_actor
+ WHERE p.facebook_likes < (SELECT AVG(p2.facebook_likes)
+                           FROM person AS p2
+                                    JOIN director d2 on p2.id_person = d2.id_director)
+ GROUP BY d.id_director, p.facebook_likes
+ HAVING COUNT(DISTINCT gm.id_genre) > 15
+ )
+ ORDER BY facebook_likes DESC
