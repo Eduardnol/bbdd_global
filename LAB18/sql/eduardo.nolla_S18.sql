d94848b65df7b5316c89f5c0c504a7e7e5f8ad06
Eduardo <eduardo.nolla@outlook.com>
2020-03-27 19:14:15 +0100

    Segunda parte

--- LAB18/sql/eduardo.nolla_S18.sql
+++ LAB18/sql/eduardo.nolla_S18.sql
@@ -41,29 +41,48 @@ BEGIN
 
     DECLARE done INT DEFAULT 0;
     DECLARE time_stamp DATETIME;
-    DECLARE cur1 CURSOR FOR SELECT access_out, id_person FROM timer;
+    DECLARE time DATETIME;
+    DECLARE persona VARCHAR(5);
+    DECLARE id_user VARCHAR(5);
+    DECLARE cur1 CURSOR FOR SELECT access_in, id_person FROM timer;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
     SET time_stamp = now();
+
     OPEN cur1;
     buscaHora:
     LOOP
-        FETCH cur1 INTO id_card_in;
-
+        CALL lsZon.getId('1234A', @cosa);
+        FETCH cur1 INTO time, persona;
         IF done = 1 THEN
-            INSERT INTO timer (access_in, access_out, id_person) VALUES (time_stamp, null, (SELECT @result));
+            INSERT INTO timer (access_in, access_out, id_person) VALUES (time_stamp, null, (SELECT @cosa));
             SET wasFound = 1;
-            LEAVE buscaHora;
-        ELSE IF c
+        ELSEIF time IS NULL THEN
+
+
+            #Sesion Abierta
+        IF (HOUR(TIMEDIFF(time, time_stamp)) >= 8) THEN
+            UPDATE timer SET access_out = '2000-01-01 00:00:00' WHERE id_user = @cosa;
+            SET wasFound = 0;
+         end if;
+            #Se va a casa
+        ELSEIF TIME_TO_SEC(TIMEDIFF(time, time_stamp)) > 20 && id_user = @cosa THEN
+            UPDATE timer SET access_out = time_stamp WHERE id_user = @cosa;
+            SET wasFound = 3;
+
+            #Se va al almacen
+        ELSEIF TIME_TO_SEC(TIMEDIFF(time, time_stamp)) < 20 && id_user = @cosa THEN
+            SET wasFound = 2;
+             #Se va a la office
+        ELSE
+            SET wasFound = 1;
+
         END IF;
 
-    END LOOP buscaHora
-    $$
+    END LOOP buscaHora;
 
-
-    INSERT INTO timer (access_in, access_out, id_person) VALUES (null, null, (SELECT @result));
-
-END $$
+END
+$$
 
 
 DELIMITER ;
